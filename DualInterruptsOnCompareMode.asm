016c0f42f233f40702100b4a6603008ae6ff6bb7
John Hopkins <johnhopkins@gmx.co.uk>
2016-12-16 10:11:09 +0000

    Update DualInterruptsOnCompareMode.asm

--- DualInterruptsOnCompareMode.asm
+++ DualInterruptsOnCompareMode.asm
@@ -8,16 +8,16 @@
 		rjmp	OutComRegB      	; COMPARE B VECTOR
 
 reset:
-		ldi		r16, 0x06
-		sts		timsk0, r16     ; SET UP TIMSK REGISTER
-		ldi		r16, 0x20
-        	out		ocr0a, r16      ; SET UP COMPARE A REGISTER
-		ldi		r16, 0x10
-		out		ocr0b, r16      ; SET UP COMPARE B REGISTER
-		ldi		r16, 0x02
-		out		tccr0a, r16     ; SET COUNTER MODE TO COMPARE
-		ldi		r16, 0x01
-		out		tccr0b, r16     ; SET COUNTER TO NO PRESCALING AND START
+		ldi	r16, 0x06
+		sts	timsk0, r16     	; SET UP TIMSK REGISTER
+		ldi	r16, 0x20
+        	out	ocr0a, r16      	; SET UP COMPARE A REGISTER
+		ldi	r16, 0x10
+		out	ocr0b, r16      	; SET UP COMPARE B REGISTER
+		ldi	r16, 0x02
+		out	tccr0a, r16     	; SET COUNTER MODE TO COMPARE
+		ldi	r16, 0x01
+		out	tccr0b, r16     	; SET COUNTER TO NO PRESCALING AND START
 		sei                   		; ENABLE GLOBAL INTERRUPTS
 		rjmp	main_loop
 
